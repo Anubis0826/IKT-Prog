08a85f40c06943069be68c6af25d03b50b7f31ea
Anubis0826 <palinkamester0826@gmail.com>
2022-03-21 13:01:18 +0100

    03.21.

--- 03.16.py
+++ 03.16.py
@@ -1,10 +1,23 @@
 from tkinter import *
 ablak1 = Tk()
-yoker = 'H:\\IKT Prog\\'
+gyoker = 'H:\\IKT Prog\\'
 ablak1.geometry('600x600')
-ablak1.title('Boldog nő napot!')
+ablak1.title('tk')
 icon= PhotoImage(file=gyoker+'red.png')
 ablak1.iconphoto(True, icon)
-ablak1.config(background='red')
-elsokep=PhotoImage(file=gyoker+'anu.gif')
-gombkep=PhotoImage(file=gyoker+'anu.gif', width='50', height='50')
+ablak1.config(background='white')
+elsokep=PhotoImage(file=gyoker+'lámpa.png')
+gombkep=PhotoImage(file=gyoker+'lámpa.png', width='50', height='50')
+cimke = Label(ablak1,
+                fg='#000000',
+                bg='#c3cee0',
+                font=('Arial', 45, 'bold'),
+                bd=10,
+                relief=RAISED,
+                padx=25,
+                pady=30,
+                image=elsokep,
+                compound='center'
+                ).pack()
+
+ablak1.mainloop()
