20aad8cae8f36be5286578724f1d2a4fec06fa50
Anubis0826 <palinkamester0826@gmail.com>
2022-03-21 13:25:51 +0100

    03.21. 2.

--- 03.16.py
+++ 03.16.py
@@ -1,16 +1,21 @@
 from tkinter import *
 ablak1 = Tk()
-gyoker = 'H:\\IKT Prog\\'
+ablak = Tk()
+mezo1=Entry(ablak)
+mezo1.grid(row=0, column=2)
+mezo1felirat=Label(ablak, text="Első mező:")
+mezo1felirat.grid(row=0, column=1)
+
+
+kep = 'H:\\IKT Prog\\'
 ablak1.geometry('600x600')
 ablak1.title('tk')
-icon= PhotoImage(file=gyoker+'red.png')
+icon= PhotoImage(file=kep+'red.png')
 ablak1.iconphoto(True, icon)
 ablak1.config(background='white')
-elsokep=PhotoImage(file=gyoker+'lámpa.png')
-gombkep=PhotoImage(file=gyoker+'lámpa.png', width='50', height='50')
+elsokep=PhotoImage(file=kep+'lámpa.png')
+gombkep=PhotoImage(file=kep+'lámpa.png', width='50', height='50')
 cimke = Label(ablak1,
-                fg='#000000',
-                bg='#c3cee0',
                 font=('Arial', 45, 'bold'),
                 bd=10,
                 relief=RAISED,
@@ -20,4 +25,6 @@ cimke = Label(ablak1,
                 compound='center'
                 ).pack()
 
-ablak1.mainloop()
+
+
+ablak.mainloop()
